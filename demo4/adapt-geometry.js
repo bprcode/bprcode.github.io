68da188f6a8490cca17cfc8679a15b887653842c
bprcode <bprcode@gmail.com>
2023-02-12 21:06:08 -0800

    Add specular cube demo

--- demo4/adapt-geometry.js
+++ demo4/adapt-geometry.js
@@ -45,6 +45,7 @@ function buildGeometry () {
   geometry.triCube = extrudeLineStrip(geometry.quadLoop, 0, 0, 2.0)
 
   geometry.tesseract = wireframeTesseract()
+  geometry.donutTesseract = openTesseract()
 
   geometry.triCube.replace(breakQuad, 12)
   geometry.triCube.push(
@@ -171,6 +172,99 @@ function fanClose (strip) {
   }
   tris.stride = 3
   return tris
+}
+
+/**
+ * Generate a mesh for a tesseract spanning (-1,-,1,-1,-1) to (1,1,1,1).
+ * This version produces a thin frame with a central gap in every face.
+ * @returns {Mesh} An array of numbers describing vertices for a tesseract
+ * to be drawn using triangles.
+ */
+function openTesseract () {
+  const face = squareDonut
+  const tesseract = new Mesh
+  //          x  y  z  w
+  const a = [-1, 1, 1, 1]
+  const b = [-1,-1, 1, 1]
+  const c = [ 1,-1, 1, 1]
+  const d = [ 1, 1, 1, 1]
+  const e = [ 1, 1,-1, 1]
+  const f = [ 1,-1,-1, 1]
+  const g = [-1,-1,-1, 1]
+  const h = [-1, 1,-1, 1]
+
+  const l = [-1, 1, 1,-1]
+  const m = [-1,-1, 1,-1]
+  const n = [ 1,-1, 1,-1]
+  const o = [ 1, 1, 1,-1]
+  const p = [ 1, 1,-1,-1]
+  const q = [ 1,-1,-1,-1]
+  const r = [-1,-1,-1,-1]
+  const s = [-1, 1,-1,-1]
+
+  // w+ surface cube
+  face(a,b,c,d)
+  face(a,h,g,b)
+  face(h,e,f,g)
+  face(e,d,c,f)
+  face(a,d,e,h)
+  face(c,b,g,f)
+
+  // w- surface cube
+  face(l,m,n,o)
+  face(s,r,m,l)
+  face(s,p,q,r)
+  face(o,n,q,p)
+  face(s,l,o,p)
+  face(m,r,q,n)
+  
+  // cell boundaries around y-axis
+  face(a,l,m,b)
+  face(d,o,n,c)
+  face(e,p,q,f)
+  face(h,s,r,g)
+
+  // xz-parallel cell boundaries on y = -1
+  face(b,m,n,c)
+  face(g,r,m,b)
+  face(f,q,r,g)
+  face(c,n,q,f)
+
+  // xz-parallel cell boundaries on y = +1
+  face(a,l,o,d)
+  face(h,s,l,a)
+  face(e,p,s,h)
+  face(d,o,p,e)
+
+  function squareDonut (v0, v1, v2, v3) {
+    const t = 0.04
+    const center = []
+
+    for (let i = 0; i < 4; i++) {
+      center[i] = (v0[i] + v1[i] + v2[i] + v3[i]) / 4
+    }
+
+    for (const [a,b] of [[v0, v1], [v1, v2], [v2, v3], [v3, v0] ]) {
+      const aInner = []
+      const bInner = []
+
+      for (let j = 0; j < 4; j++) {
+        aInner[j] = a[j]*(1-t) + center[j]*t
+      }
+      for (let j = 0; j < 4; j++) {
+        bInner[j] = b[j]*(1-t) + center[j]*t
+      }
+
+      tesseract.push(...[
+        a, b, aInner,
+        aInner, b, bInner,
+      ].flat())
+
+    }
+  }
+
+  tesseract.stride = 4
+  return tesseract
 }
 
 /**
