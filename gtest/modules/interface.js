a4bb41d3e5b63e835ad71dfb5d107b00240b6132
bprcode <bprcode@gmail.com>
2023-05-20 19:31:39 -0700

    Test layout reliability

---
+++ gtest/modules/interface.js
@@ -0,0 +1,181 @@
+'use strict';
+// Copyright © 2023 Bryan Rauen.
+// All rights reserved. https://bprcode.github.io/
+
+import { beginClarityTransition } from "./tesseract-controller.js"
+
+const log = console.log.bind(console)
+const el = document.getElementById.bind(document)
+const select = document.querySelector.bind(document)
+const all = document.querySelectorAll.bind(document)
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', initialize)
+} else {
+  queueMicrotask(initialize)
+}
+
+function initialize () {
+  const underline = select('.underline')
+  let lastMove = 'left'
+  let bounds = { left: 0, right: 0, top: 0, bottom: 0 }
+  let lastHoveredBox = select('.link-box')
+
+  updateContainerBounds()
+  const links = all('.link-box a')
+  underline.style['right'] = bounds.right - links[links.length - 1]
+    .getBoundingClientRect().right + 'px'
+
+  for (const box of all('.link-box')) {
+    box.addEventListener('pointerenter', event => {
+      const anchor = box.querySelector('.line-link')
+      lastHoveredBox = box
+
+      underline.classList.remove('no-delay')
+
+      if (anchor.getBoundingClientRect().left - bounds.left
+            < parseFloat(getComputedStyle(underline).left)) {
+        underline.classList.remove('delay-left')
+        underline.classList.add('delay-right')
+        lastMove = 'left'
+      } else {
+        underline.classList.remove('delay-right')
+        underline.classList.add('delay-left')
+        lastMove = 'right'
+      }
+
+      underline.style.left =
+        anchor.getBoundingClientRect().left - bounds.left + 'px'
+
+      underline.style.right =
+        bounds.right - anchor.getBoundingClientRect().right + 'px'
+    })
+  }
+
+  select('.link-box-container')
+    .addEventListener('pointerleave', () => {
+    underline.classList.remove('delay-left')
+    underline.classList.remove('delay-right')
+    underline.classList.add('no-delay')
+
+    if (lastMove === 'left') {
+      underline.style.right = bounds.right
+        - lastHoveredBox.querySelector('a')
+          .getBoundingClientRect().left + 'px'
+    } else {
+      underline.style.left =
+        lastHoveredBox.querySelector('a')
+          .getBoundingClientRect().right - bounds.left + 'px'
+    }
+  })
+
+  function updateContainerBounds () {
+    bounds = select('.link-box-container')
+      .getBoundingClientRect()
+  }
+
+  function ejectUnderline () {
+    const links = all('.link-box a')
+
+    underline.classList.remove('delay-right')
+    underline.classList.remove('delay-left')
+    underline.classList.add('no-delay')
+    underline.style.left = '100%'
+    underline.style['right'] = bounds.right - links[links.length - 1]
+      .getBoundingClientRect().right + 'px'
+  }
+
+  window.addEventListener('resize', () => {
+    updateContainerBounds()
+    ejectUnderline()
+  })
+
+  // Single-page link reactions:
+  for (const box of all('.link-box')) {
+    box.addEventListener('click', event => {
+      const shineAnimationTime = 1100
+      const section = box.dataset.section
+      const content = select('.' + section)
+      const shineContainer = content.querySelector('.shine-container')
+      content.classList.remove('concealed')
+      content.classList.add('opaque')
+      content.scrollTop = 0
+
+      shineContainer.classList.add('display-block')
+      setTimeout(() => {
+        shineContainer.classList.remove('display-block')
+      }, shineAnimationTime)
+
+      for (const c of all('.content')) {
+        if (!c.classList.contains(section)) {
+          c.classList.remove('opaque')
+          c.classList.add('concealed')
+        }
+      }
+
+      glint(content.querySelector('.shine'))
+
+      // Blur the tesseract rendering to improve text overlay legibility:
+      beginClarityTransition(0, 1000)
+    })
+
+    box.addEventListener('pointerenter', event => {
+      select('.underline').classList.add('bright-underline')
+    })
+
+    box.addEventListener('pointerleave', event => {
+      select('.underline').classList.remove('bright-underline')
+    })
+  }
+
+  // Close content when clicking outside
+  document.body.addEventListener('click', event => {
+    // Check whether the click was within a content section:
+    for (const e of all('.content')) {
+      if (e.contains(event.target) || e === event.target) {
+        // Click was within content, do not close.
+        return
+      }
+    }
+
+    // Do not close content when clicking a same-page link:
+    if (event.target.classList.contains('link-box')
+      || event.target.classList.contains('line-link')) {
+      const q =
+        select('.' + event.target.dataset.section
+                                + ' .shine')
+      // Respond by playing a glint animation
+      glint(q)
+      return
+    }
+
+    for (const e of all('.content')) {
+      e.classList.remove('opaque')
+      e.classList.add('concealed')
+    }
+
+    for (const e of all('.shine')) {
+      e.classList.remove('shine-reveal')
+    }
+
+    // Restore the rendering clarity factor to its normal value:
+    beginClarityTransition(1, 1000)
+  })
+
+  let glintLockout = false
+  function glint (shinyElement) {
+    if (glintLockout) { return }
+    glintLockout = true
+    setTimeout(() => {
+      glintLockout = false
+    }, 1000)
+
+    for (const e of all('.shine')) {
+      e.classList.remove('shine-reveal')
+    }
+
+    setTimeout(() => {
+      shinyElement.classList.add('shine-reveal')
+    }, 200)
+  }
+}
